645a27cb52656c9f3a2fdd6729dfc6a53fc8ef9f
ZwkAlex <zhengwukunalex@gmail.com>
2020-05-25 18:11:13 +0800

    Initial commit

---
+++ app/src/main/java/com/zwk/trash_detective/Activity/ShowUpActivity.java
@@ -0,0 +1,43 @@
+package com.zwk.trash_detective.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zwk.trash_detective.Bean.Block;
+import com.zwk.trash_detective.R;
+
+import java.util.ArrayList;
+
+public class ShowUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private mSurfaceView mSurfaceView;
+    private ArrayList<Block> blocks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showup);
+        findViewById(R.id.back_to_img).setOnClickListener(this);
+        mSurfaceView = findViewById(R.id.surface_view);
+        ArrayList<Block> blocks = getIntent().getParcelableArrayListExtra("blocks");
+        mSurfaceView.drawBlocks(blocks);
+        this.blocks=blocks;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSurfaceView.drawBlocks(blocks);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.back_to_img){
+            finish();
+        }
+    }
+}
